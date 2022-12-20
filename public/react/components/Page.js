72aaf2aa1770a04463133ef784d074047e617ce6
Annalise Grekalski <grekalski842@gmail.com>
2022-12-20 13:29:26 -0500

    Page.js fetch requests set up

--- public/react/components/Page.js
+++ public/react/components/Page.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import apiURL from "../api";
 
 export const Page = (props) => {
 
+  //creaating state for data to be displayed
+  [currentData, setCurrentData] = useState(undefined);
+
+  //handleClick function to fetch data and set the state above
+  const handleClick = async () => {
+    const response = await fetch(`${apiURL}/wiki/${props.page.slug}`);
+    const data = await response.json();
+    console.log(data)
+    setCurrentData(data);
+  }
+
   return <>
-    <h3>{props.page.title}</h3>
+    <h3 onClick={ handleClick }>
+      {props.page.title}
+      </h3>
+      <div>
+        {currentData ? currentData.content : ""}
+      </div>
   </>
 } 
 	
